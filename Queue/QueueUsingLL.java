7918fe5d887624c67a4fe7828256984af8038aee
Ritam Das <106969260+ritam332@users.noreply.github.com>
2023-04-28 17:32:54 +0530

    Update QueueUsingLL.java

--- Queue/QueueUsingLL.java
+++ Queue/QueueUsingLL.java
@@ -10,6 +10,7 @@ public class QueueUsingLL {
 			this.data=data;
 			next=null;
 		}
+	}
 		
 		
 	static class Queue{
@@ -23,12 +24,51 @@ public class QueueUsingLL {
 		
 		public static void add(int data) {
 			Node n= new Node(data);
-			if (tail==null) {
+			if (isEmpty()) {
 				tail=head=n;
 				return ;
+			}else {
+				tail.next=n;
+				tail=n;
 			}
 		}
-	}
-	    
+		
+		public static int remove() {
+			if (isEmpty()) {
+				System.out.println("Empty Queue"); return -1;
+			}
+			
+			int front=head.data;
+			
+			//single node
+			
+			if (head==tail) {
+				tail=null;
+			}
+			head=head.next;
+			return front;
+		}
+		public static int peek() {
+			if (isEmpty()) {
+				System.out.println("Empty Queue"); 
+				return -1;
+			}
+			
+			return head.data;
+		}
+		
+    }
+	
+	
+	
+	public static void main(String[] args) {
+		 Queue q= new Queue();
+		for(int i=0;i<5;i++) {
+			q.add(i);
+		}
+		while(!q.isEmpty()) {
+			System.out.println(q.peek());
+			q.remove();
+		}
 	}
 }
